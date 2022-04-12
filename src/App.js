9150304d608e198044fa5737842bec35713b04be
Monchusay <armydolgov@mail.ru>
2022-04-12 05:38:10 +0300

    0.1.0

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ import style from "./App.module.css"
 
 const App = (props) => {
 
-    const [todo, setTodo] = useState()
+    const [todo, setTodo] = useState("")
+    const [todos, setTodos] = useState([])
+
+    const handleAdd = (e) => {
+        e.preventDefault()
+        if(todo){
+            setTodos([...todos, {id:Math.random(), todo, isDone:false}])
+            setTodo("")
+        }
+    }
+    console.log(todos)
 
   return (
     <div className={style.App}>
       <div className={style.Tasks}>
         <span> Tasks </span>
       </div>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 };
--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,13 +1,23 @@
 import * as React from "react";
-import style from "./InputField.module.css"
+import style from "./InputField.module.css";
+import {useRef} from "react";
 
-const InputField = (todo, setTodo) => {
-  return  (
-    <form className={style.input}>
-        <input  placeholder="Enter a task" className={style.inputBox}/>
-        <button className={style.inputSubmit}> GO </button>
-  </form>
-)
+const InputField = ({todo, setTodo, handleAdd}) => {
+
+    const inputRef = useRef(null)
+
+  return (
+    <form className={style.input} onSubmit={(e) => handleAdd(e)}>
+      <input
+          ref={inputRef}
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className={style.inputBox}
+      />
+      <button className={style.inputSubmit}> GO </button>
+    </form>
+  );
 };
 
-export default InputField
+export default InputField;
