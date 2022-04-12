4f1eff6532ba849f88ac65f34291b8606246a629
Monchusay <armydolgov@mail.ru>
2022-04-12 03:05:50 +0300

    111

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.module.css";
+import React from "react";
+import style from "./App.module.css";
+
+const App: React.FC = () => {
+  return (
+    <div className={style.Tasks}>
+      <div>
+        <span> Tasks </span>
+      </div>
+    </div>
+  );
+};
+
+export default App;
